289ecce2c776831f76f0714c15480e7d14b5370f
Vova1982 <64212894+Vova1982@users.noreply.github.com>
2024-01-11 23:02:50 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -14,12 +14,29 @@
 //     console.log(a);
 // }
 
-let a = document.querySelectorAll('.one');
-for (i = 0; i < a.length; i++){
-  a[i].style.background = "red";
-    console.log(i);
-    a[i].onclick = two;
+// let a = document.querySelectorAll('.one');
+// for (i = 0; i < a.length; i++){
+//   a[i].style.background = "red";
+//     console.log(i);
+//     a[i].onclick = two;
+// }
+// function two() {
+//     console.log("Red Bull")
+// }
+document.querySelector('.btn_22').onclick = () => { 
+    let r_1 = document.querySelectorAll('.radio_1');
+    // console.log(r_1);
+    for (i = 0; i < r_1.length; i++){
+        if (r_1[i].checked) {
+            console.log(r_1[i].value);  
+ }
+    }
 }
-function two() {
-    console.log("Red Bull")
+function t_99(){
+   let a_99 = document.querySelector('.out_99');
+   let b = '_';
+    for (let i = 1; i < 15; i++) {
+        a_99.innerHTML += [i]+ b;
+    }
 }
+document.querySelector('#btn_99').onclick = t_99;
